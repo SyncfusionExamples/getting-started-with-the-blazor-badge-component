c5eb2d685bd0654157f4696727ada18a0d67c315
Mahesh Palanisamy <mahesh.palanisamy@syncfusion.com>
2023-06-01 10:04:29 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using BlazorApp_Badge;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddSyncfusionBlazor();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjEwMzk1M0AzMjMxMmUzMjJlMzVMMWVHZkpZLzNvaFdqamxDdUFmWVNUaEJrTHc5OElpRDc2Sm4ydUxFVVNVPQ==");
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
